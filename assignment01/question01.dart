05558b050e7eb54c39c38a1e34e3cd00c0f0a7f5
MahadAnsari18 <mahadansari18@gmail.com>
2025-05-05 02:09:23 +0500

    add assignment01

---
+++ assignment01/question01.dart
@@ -0,0 +1,18 @@
+/* • You have to print your introduction, 
+   • Assign your Name , Age , Class , percentage , previous result , according to their data types. */
+
+void main() {
+  String name = "Muhammad Mahad";
+  int age = 19;
+  int grade = 11;
+  num percentage = 78.5;
+  String previousResult = "A";
+  print(
+    "My name is $name and I'm $age years old. I am a student of grade $grade "
+    "and I've achieved $percentage percentage this semester and my previous result was $previousResult grade",
+  );
+}
+
+// Output is:
+//My name is Muhammad Mahad and I'm 19 years old. I am a student of grade 11 and I've achieved 78.5 percentage this semester
+// and my previous result was A grade
